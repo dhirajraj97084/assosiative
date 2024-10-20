74a48014dee87be9666e3dace6f127f3dc8aff2a
Dhiraj-Tiwari <rajtiwari73011@gmail.com>
2024-10-20 19:59:21 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+$students=[
+    "name"=>"aman",
+    "email"=>"aman@gmail.com",
+    "score"=>85
+];
+echo $students["email"];
+echo "<br>";
+echo $students["name"];
+echo "<br>";
+echo $students["score"];
+
+
+$arr=["red","blue","black"];
+echo "<br>";
+echo $arr[2];
+?>
